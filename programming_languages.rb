98100192c7f5a1e885b146e9b612eb9bb5dcde49
Ryan Daley <rdaley321@yahoo.com>
2018-11-11 09:13:04 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,9 +1,9 @@
 def reformat_languages(languages)
   new_hash = Hash.new
-  languages.each do |type,language|
-    new_hash[type] = language
-    language.each do |key,value|
-      puts key
+  languages.each do |type,languages_hash|
+    new_hash[type] = languages_hash
+    languages_hash.each do |language,value|
+      puts value
     end
   end
   new_hash
